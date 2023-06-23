e5d05ab340eefdfefd99b21e43c4f7c087fbbf13
snehalbabar <h728e344@wichita.edu>
2023-06-23 12:56:41 -0500

    Adding Singleton example

---
+++ SignletonPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using SingletonPattern;
+
+Singleton employee = Singleton.getInstance;
+employee.printDetails("Hello from employee");
+
+Singleton student = Singleton.getInstance;
+student.printDetails("Hello from student");
+Console.Read();
+
---
+++ SignletonPattern/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace SingletonPattern
+{
+	public sealed class Singleton
+	{
+		private static int counter = 0;
+		public static Singleton instance = null;
+
+		public static Singleton getInstance {
+			get {
+				if (instance == null)
+				{
+					instance = new Singleton();
+				}
+				return instance;
+			} 
+
+
+		}
+
+		private Singleton()
+		{
+			counter++;
+			Console.WriteLine("Counter :" + counter);
+		}
+
+		public void printDetails(string msg)
+		{
+			Console.WriteLine(msg);
+			
+		}
+	}
+}
+
